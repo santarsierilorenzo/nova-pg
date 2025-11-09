f2eb2a18d6674651cbd89968f2fdb1163227a420
santarsierilorenzo <lorenzo.santarsieri@outlook.com>
2025-11-09 11:29:54 +0000

    chore: configuration files update

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git update-index --assume-unchanged .devcontainer/devcontainer.json.example
+git update-index --assume-unchanged .devcontainer/devcontainer.json
+git update-index --assume-unchanged .devcontainer/Dockerfile
+git update-index --assume-unchanged requirements.txt
+git update-index --assume-unchanged .dockerignore
+git update-index --assume-unchanged .gitignore
+git update-index --assume-unchanged setup.sh
